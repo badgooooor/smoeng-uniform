7424206ec6908a4372a553f81688f6160c368bda
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-07-11 18:17:23 +0700

    Add qr code

--- src/components/SummaryDialog.tsx
+++ src/components/SummaryDialog.tsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 import { Dialog, Button, Box, DialogTitle, DialogContent, DialogActions } from "@material-ui/core"
-
-interface Product {
-  product: string;
-  size: string;
-  amount: number;
-}
+import { QRCode } from "react-qr-svg";
+import Product from "../types/product";
 
 interface Props {
   open: boolean;
@@ -14,32 +10,63 @@ interface Props {
 }
 
 export default function SummaryDialog(props: Props) {
+  const [confirm, setConfirm] = React.useState(false);
+  const [orderId, setOrderId] = React.useState("order.id");
+
   const handleClose = () => {
     props.onClose();
+    setConfirm(false);
   }
 
   return (
     <Dialog open={props.open} onClose={handleClose}>
       <DialogContent>
-        <Box p={2}>
-          <h2>สรุปคำสั่งซื้อ</h2>
-          <Box>
-            <ul>
-              {props.orders.map((order, index) => (
-                <li>{order.product} ขนาด {order.size} จำนวน {order.amount} ตัว</li>
-              ))}
-            </ul>
+        {!confirm ? (
+          <Box p={2}>
+            <h2>สรุปคำสั่งซื้อ</h2>
+            <Box>
+              <ul>
+                {props.orders.map((order, index) => (
+                  <li>{order.product} ขนาด {order.size} จำนวน {order.amount} ตัว</li>
+                ))}
+              </ul>
+            </Box>
+            <em>กรุณาตรวจสอบรายการก่อนยืนยันการสั่งซื้อ</em>
           </Box>
-          <em>กรุณาตรวจสอบรายการก่อนยืนยันการสั่งซื้อ</em>
-        </Box>
+        ) : (
+            <Box>
+              <h2>QR Code สำหรับรับสินค้าที่สั่งที่สโมสรนักศึกษา</h2>
+              <QRCode
+                bgColor="#FFFFFF"
+                fgColor="#000000"
+                level="Q"
+                style={{ width: 256 }}
+                value={orderId}
+              />
+              <em>กรุณาถ่ายภาพหน้าจอเพื่อ checkout ให้กับหน้าร้าน</em>
+            </Box>
+          )}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleClose}>
-          ยกเลิก
-        </Button>
-        <Button color="primary">
-          ยืนยัน
-        </Button>
+        {
+          !confirm ? (
+            <>
+              <Button onClick={handleClose}>
+                ยกเลิก
+              </Button>
+              <Button color="primary" onClick={() => {
+                setConfirm(true);
+              }}>
+                ยืนยัน
+              </Button>
+            </>
+          ) : (
+              <Button onClick={handleClose}>
+                ปิด
+              </Button>
+            )
+        }
+
       </DialogActions>
     </Dialog>
   )
