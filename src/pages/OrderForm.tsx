43726c7ca7f4b1d7be9ae99aa9ea30806acd3410
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-07-11 13:26:59 +0700

    Update layout

--- src/pages/OrderForm.tsx
+++ src/pages/OrderForm.tsx
@@ -3,13 +3,18 @@ import { Formik, Form, Field, FieldArray } from 'formik';
 import Layout from "../containers/Layout"
 import { Select } from 'material-ui-formik-components/Select'
 import { TextField } from "material-ui-formik-components/TextField";
+import { Button, Grid } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 interface OrderFormValue {
   status: string;
   orders: Array<Object>;
 }
 
+const useStyles = makeStyles((theme: Theme) => createStyles({}));
+
 export default function OrderForm() {
+  const classes = useStyles();
   const initialValues: OrderFormValue = { status: "unpaid", orders: [] };
 
   return (
@@ -29,57 +34,69 @@ export default function OrderForm() {
                   <div>
                     {values.orders && values.orders.length > 0 ? (
                       values.orders.map((order, index) => (
-                        <div key={index}>
-                          <Field
-                            required
-                            component={Select}
-                            name={`orders.${index}.product`}
-                            label="สินค้า"
-                            options={[
-                              { value: "เสื้อชอป", label: "เสื้อชอป" },
-                              { value: "เสื้อโปโล", label: "เสื้อโปโล" }
-                            ]}
-                            variant="outlined"
-                          />
-                          <Field
-                            required
-                            component={Select}
-                            name={`orders.${index}.size`}
-                            label="ขนาด"
-                            options={[
-                              { value: "S", label: "S" },
-                              { value: "M", label: "M" },
-                              { value: "L", label: "L" },
-                              { value: "XL", label: "XL" }
-                            ]}
-                            variant="outlined"
-                          />
-                          <Field name={`orders.${index}.amount`} type="number" label="จำนวน" component={TextField} />
-                          <button
-                            type="button"
-                            onClick={() => arrayHelpers.remove(index)}
-                          >
-                            -
-                          </button>
-                          <button
-                            type="button"
-                            onClick={() => arrayHelpers.insert(index, '')} // insert an empty string at a position
-                          >
-                            +
-                          </button>
+                        <div>
+                          <Grid container spacing={2} alignItems="center" key={index}>
+                            <Grid item xs={3}>
+                              <Field
+                                required
+                                component={Select}
+                                name={`orders.${index}.product`}
+                                label="สินค้า"
+                                options={[
+                                  { value: "เสื้อชอป", label: "เสื้อชอป" },
+                                  { value: "เสื้อโปโล", label: "เสื้อโปโล" }
+                                ]}
+                                variant="outlined"
+                              />
+                            </Grid>
+                            <Grid item xs={3}>
+                              <Field
+                                required
+                                component={Select}
+                                name={`orders.${index}.size`}
+                                label="ขนาด"
+                                options={[
+                                  { value: "S", label: "S" },
+                                  { value: "M", label: "M" },
+                                  { value: "L", label: "L" },
+                                  { value: "XL", label: "XL" }
+                                ]}
+                                variant="outlined"
+                              />
+                            </Grid>
+                            <Grid item xs={3}>
+                              <Field
+                                name={`orders.${index}.amount`}
+                                type="number"
+                                label="จำนวน"
+                                component={TextField}
+                                variant="outlined"
+                              />
+                            </Grid>
+                            <Grid item xs={3}>
+                              <Button onClick={() => arrayHelpers.remove(index)}>
+                                -
+                              </Button>
+                            </Grid>
+                          </Grid>
                         </div>
                       ))
                     ) : (
-                        <button type="button" onClick={() => arrayHelpers.push('')}>
-                          Add order
-                        </button>
+                        <></>
                       )}
+                    <Button onClick={() => arrayHelpers.push('')} fullWidth>
+                      เพิ่มออร์เดอร์
+                    </Button>
+                    <Button
+                      type="submit"
+                      disabled={values.orders.length == 0}
+                      fullWidth
+                    >
+                      สั่งซื้อ
+                    </Button>
                   </div>
                 )}
               />
-              <div>
-                <button type="submit">Submit</button>
-              </div>
             </Form>
           )}
         />
