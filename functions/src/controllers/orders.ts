70e5883bc2b764ea920f0fcacd974d86491d8b6f
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-07-18 11:25:23 +0700

    Add order routes and controllers

---
+++ functions/src/controllers/orders.ts
@@ -0,0 +1,41 @@
+import admin from '../firebase.utils';
+
+const db = admin.firestore();
+
+export async function getOrders(userId: string) {
+  let orders = await db.collection("users")
+    .doc(userId)
+    .collection("orders")
+    .get()
+    .then((snapshot) =>
+      snapshot.docs.map((doc) => {
+        let data = doc.data();
+        data.id = doc.id;
+        return data;
+      })
+    );
+
+  return orders;
+}
+
+export async function createOrder(userId: string, data: Object) {
+  const ref = await db.collection("users").doc(userId).collection("orders").add(data);
+  return ref.id;
+}
+
+export async function updateOrder(userId: string, orderId: string, data: Object) {
+  const ref = await db.collection("users").doc(userId).collection("orders").doc(orderId).update(data);
+  return ref.writeTime;
+}
+
+export async function deleteOrder(userId: string, orderId: string) {
+  const ref = await db.collection("users").doc(userId).collection("orders").doc(orderId).delete();
+  return ref.writeTime;
+}
+// module.exports.updateOrder = (userId, orderId, data) => {
+
+// }
+
+// module.exports.deleteOrder = (userId, orderId) => {
+
+// }
---
+++ functions/src/routes/orders.ts
@@ -0,0 +1,46 @@
+import * as express from "express"
+import * as OrderController from "../controllers/orders";
+import * as admin from "firebase-admin"
+
+const router: express.IRouter = express.Router();
+
+router.get("/:userId", async (request: express.Request, response: express.Response) => {
+  const data = await OrderController.getOrders(request.params.userId);
+  console.log(data)
+  return response.send(data)
+})
+
+router.post("/:userId", async (request: express.Request, response: express.Response) => {
+  let data = request.body;
+  data.createdAt = admin.firestore.Timestamp.fromDate(new Date());
+  const ref = await OrderController.createOrder(request.params.userId, request.body);
+  return response.send({
+    message: "Create new user's order",
+    ref: ref
+  })
+})
+
+router.put("/:userId/:orderId", async (request: express.Request, response: express.Response) => {
+  let data = request.body;
+  data.updatedAt = admin.firestore.Timestamp.fromDate(new Date());
+  const updated = await OrderController.updateOrder(request.params.userId, request.params.orderId, request.body).catch((err) => console.log(err));
+
+  return response.send({
+    message: "Update orders from id",
+    userId: request.params.userId,
+    orderId: request.params.orderId,
+    updatedTime: updated
+  })
+})
+
+router.delete("/:userId/:orderId", async (request: express.Request, response: express.Response) => {
+  const deleted = await OrderController.deleteOrder(request.params.userId, request.params.orderId)
+  return response.send({
+    message: "Delete orders from id",
+    userId: request.params.userId,
+    orderId: request.params.orderId,
+    deletedTime: deleted
+  })
+})
+
+export default router;
