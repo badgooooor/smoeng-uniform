87dbf9585ac8a4bfcef1154abb854917cd460eab
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-07-18 19:18:22 +0700

    Add profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,10 +1,30 @@
 import * as React from "react"
+import { observer } from "mobx-react";
+import { user_store } from "../stores/user";
 import Layout from "../containers/Layout";
+import { Grid } from "@material-ui/core";
+import EditIcon from '@material-ui/icons/Edit';
 
-export default function Settings() {
+import Avatar from "../components/Profile/Avatar";
+import Contact from "../components/Profile/Contact";
+
+const Profile = observer(() => {
   return (
     <Layout>
-      <p>Profile</p>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={4}>
+          <Avatar
+            photoUrl={user_store.photoUrl}
+            displayName={user_store.displayName}
+            email={user_store.email}
+          />
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Contact />
+        </Grid>
+      </Grid>
     </Layout>
   );
-}
+})
+
+export default Profile;
