25c365647d092c4124fda5c9771aab42e1909c04
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2020-07-18 08:38:36 +0700

    Add pages to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import * as React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import OrderForm from "./pages/OrderForm";
-import Home from "./pages/Home";
 import { ThemeProvider } from "@material-ui/core/styles";
+
+import Home from "./pages/Home";
+import OrderForm from "./pages/OrderForm";
+import OrderList from "./pages/OrderList";
 import theme from "./styles/theme";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Route exact path="/" component={Home} />
-        <Route exact path="/newOrder" component={OrderForm} />
+        <Route exact path="/neworder" component={OrderForm} />
+        <Route exact path="/orders" component={OrderList} />
+        <Route exact path="/settings" component={Settings} />
       </BrowserRouter>
     </ThemeProvider>
   );
 }
+
 export default App;
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "../containers/Layout";
+
+export default function Settings() {
+  return (
+    <Layout>
+      <p>Settings</p>
+    </Layout>
+  );
+}
